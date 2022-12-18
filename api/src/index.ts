cd19b665c6c5dc8aac73629cbb3de758b826107a
Ramon Nunes <ramontsdev@gmail.com>
2022-12-18 16:56:18 -0300

    implementando websocket com socket.io

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,17 @@
-import path from 'node:path';
-
 import express from 'express';
 import mongoose from 'mongoose';
+import http from 'node:http';
+import path from 'node:path';
+import { Server } from 'socket.io';
 
 import { router } from './router';
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
     const port = 3001;
 
     app.use((req, res, next) => {
@@ -21,7 +25,7 @@ mongoose.connect('mongodb://localhost:27017')
     app.use(express.json());
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Server is running on http:localhost:${port}`);
     });
   })
